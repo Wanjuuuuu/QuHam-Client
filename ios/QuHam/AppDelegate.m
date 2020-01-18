ce6adbca8b2e64d9e2462d4b692b78741d9f1030
Wanjuuuuu <makarong5798@gmail.com>
2020-01-18 13:48:45 +0900

    git-secret와 react-native-fbsdk 추가

--- ios/QuHam/AppDelegate.m
+++ ios/QuHam/AppDelegate.m
@@ -11,6 +11,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -19,6 +21,8 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"QuHam"
                                             initialProperties:nil];
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+  didFinishLaunchingWithOptions:launchOptions];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
